035b28aa9173881110c14b81653b6b0346b53b0d
Gideon Mrina <gidepan002@gmail.com>
2025-02-22 01:40:38 +0300

    Initial Push

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
+
+#root = tk.Tk()
+#root.withdraw()
+
+#folder = filedialog.askdirectory()
+#print(f"Selected Folder : {folder}")
+
+messagebox.showwarning("Warning", "You Have Been Hacked")
